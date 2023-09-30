bc0ec38d2fcec291bbd0cf466a0fe8cd19abc8f7
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-09-30 17:01:22 +0300

    Add QuickPick menu with lines from all files

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+async function searchActive() {
+  const lines: string[] = [];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "searchground" is now active!');
+  const files = await vscode.workspace.findFiles("**/*.ts", "**/node_modules/**", 100);
+  await Promise.all(
+    files.map(async (file) => {
+      const data = await vscode.workspace.fs.readFile(file);
+      lines.push(...data.toString().split("\r\n"));
+    })
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('searchground.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from searchground!');
-	});
+  const quickPick = vscode.window.createQuickPick();
+  quickPick.placeholder = "Search all files... (excluding **/node_modules/**)";
+  quickPick.items = lines.map((label) => ({ label }));
 
-	context.subscriptions.push(disposable);
+  quickPick.onDidHide(() => quickPick.dispose());
+  quickPick.show();
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("searchground.searchactive", searchActive);
+
+  context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
